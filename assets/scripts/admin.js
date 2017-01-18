9b8789afa797a94272e856c258ae635b904a0668
Bolaji <cooproto@gmail.com>
2017-01-18 18:32:08 +0100

    Added some extra scripts and styles

--- assets/scripts/admin.js
+++ assets/scripts/admin.js
@@ -59,12 +59,12 @@ function newMaintenanceRequest() {
   var projectRef = fire.database().ref('/cars');
   console.log(projectRef);
   projectRef.push({
-    "engine number": engineNumber,
-    "track ID": trackID,
-    "Car Model": carModel,
+    "enginenumber": engineNumber,
+    "trackID": trackID,
+    "CarModel": carModel,
     "Damage" :  damage,
-    "Car Owner Name" : ownerName,
-    "Car Owner Mobile" : contact
+    "CarOwnerName" : ownerName,
+    "CarOwnerMobile" : contact
   });
 
   document.getElementById('engineNumber').value = "";
@@ -79,4 +79,97 @@ function newMaintenanceRequest() {
 		}, function (error) {
 			console.log("Error: " +  error.code);
 		});
+    alert('Success!');
+}
+
+function createNewStaff() {
+  var name = document.getElementById('name').value;
+  var age = document.getElementById('age').value;
+  var contactNo = document.getElementById('contactNo').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var specialty = document.getElementById('specialty').value;
+  var dob = document.getElementById('dob').value;
+
+  var projectRef = fire.database().ref('/staff');
+  console.log(projectRef);
+  projectRef.push({
+    "name": name,
+    "age": age,
+    "dob": dob,
+    "contactNo": contactNo,
+    "email" : email,
+    "password" : password,
+    "specialty" : specialty
+  });
+
+  document.getElementById('name').value = "";
+  document.getElementById('age').value = "";
+  document.getElementById('dob').value = "";
+  document.getElementById('contactNo').value = "";
+  document.getElementById('email').value = "";
+  document.getElementById('password').value = "";
+  document.getElementById('specialty').value = "";
+
+  projectRef.on("value", function(snapshot){
+			console.log(snapshot.val());
+		}, function (error) {
+			console.log("Error: " +  error.code);
+		});
+}
+
+function viewLog() {
+  var projectRef = fire.database().ref('/cars');
+
+  projectRef.orderByKey().on("value", function(snapshot){
+    var cars = [];
+    snapshot.forEach(function(childSnapshot) {
+        var car = childSnapshot.val();
+        cars.push(car);
+    });
+    for (var i = 0;i < cars.length; i++) {
+      var car = cars[i];
+      var $row = $('<tr>'+
+        '<td>'+ (i+1) +'</td>'+
+        '<td>' + car.CarOwnerName + '</td>' +
+        '<td>' + car.CarOwnerMobile + '</td>' +
+        '<td>' + car.trackID + '</td>' +
+        '<td>' + car.CarModel + '</td>' +
+        '<td>' + car.enginenumber + '</td>' +
+        '<td>' + car.Damage + '</td>' +
+        '</tr>');
+
+      $('table > tbody:last').append($row);
+    }
+	}, function (error) {
+		console.log("Error: " +  error.code);
+	});
+}
+
+function viewStaff() {
+  var projectRef = fire.database().ref('/staff');
+
+  projectRef.orderByKey().on("value", function(snapshot){
+    var staffArray = [];
+    snapshot.forEach(function(childSnapshot) {
+        var staff = childSnapshot.val();
+        staffArray.push(staff);
+    });
+    for (var i = 0;i < staffArray.length; i++) {
+      var staff = staffArray[i];
+      var $row = $('<tr>'+
+        '<td>'+ (i+1) +'</td>'+
+        '<td>' + staff.name + '</td>' +
+        '<td>' + staff.age + '</td>' +
+        '<td>' + staff.dob + '</td>' +
+        '<td>' + staff.contactNo + '</td>' +
+        '<td>' + staff.email + '</td>' +
+        '<td>' + staff.specialty + '</td>' +
+        '</tr>');
+
+      $('table > tbody:last').append($row);
+    }
+	}, function (error) {
+		console.log("Error: " +  error.code);
+	});
 }
