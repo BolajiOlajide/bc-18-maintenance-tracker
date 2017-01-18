5166d90ceeb101f7ac0b405f00706e14ce5f258e
Bolaji <cooproto@gmail.com>
2017-01-18 13:23:37 +0100

    Added new route for creating new staff

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 var express = require('express');
+require('dotenv').config();
 var bodyParser = require('body-parser');
-var firebase = require('firebase');
 var app = express();
+var methodOverride = require('method-override');
+var firebase = require('./firebasedb');
+
 
 app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
+
 app.use('/assets', express.static('assets'));
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port',process.env.PORT || 3000);
 
@@ -18,6 +28,30 @@ app.get('/', function(req,res) {
   console.log(req.url);
     res.render('index');
     //res.send('Hey, Express Works!');
+});
+
+
+
+app.post('/', function(req,res) {
+    const email = req.body.username;
+    const password = req.body.password;
+
+    firebase.auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(function(user) {
+          //res.status(200).send({message: 'Login Success'});
+
+          console.log('Success!');
+          res.redirect('/admin');
+            //app.get('/admin', function(req,res) {
+              //console.log(req.url);
+                //res.render('admin');
+            //});
+        })
+        .catch(function(error){
+          console.log(error);
+          res.status(500).send({message: 'Login Failed'});
+        });
 });
 
 app.get('/createAccount', function(req,res) {
@@ -34,13 +68,27 @@ app.use(function(req, res) {
     res.status(500).send('500: Internal Server Error');
 }); */
 
-app.get('/contact', function(req,res) {
-  console.log(req.url);
-    res.render('contact');
-    //res.send('Hey, Express Works!');
-});
-
 app.get('/admin', function(req,res) {
   console.log(req.url);
   res.render('admin');
+});
+
+app.get('/track', function(req,res) {
+  console.log(req.url);
+  res.render('track');
+});
+
+app.get('/report', function(req,res) {
+  console.log(req.url);
+  res.render('report');
+});
+
+app.get('/newMaintain', function(req,res) {
+  console.log(req.url);
+  res.render('newMaintain');
+});
+
+app.get('/newStaff', function(req,res) {
+  console.log(req.url);
+  res.render('newStaff');
 });
